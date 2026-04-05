58aca18030f5f5a592b6c4092d050ead472b5aa5
Pratama Dimas <pratamadimas.pw@gmail.com>
2026-04-05 11:37:27 +0700

    💄 style: adjust globals dark grid selector and metadata text

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,5 +3,5 @@ import { type Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Get started with Next.js',
   description:
-    'A production-ready starter with TypeScript, Tailwind CSS v4, HeroUI, React Compiler Jest, ESLint, and Prettier — configured and ready to go.',
+    'A production-ready starter with TypeScript, Tailwind CSS v4, HeroUI, React Compiler, Jest, ESLint, and Prettier — configured and ready to go.',
 };
